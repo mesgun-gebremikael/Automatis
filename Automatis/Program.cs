1cbeab5884117fd7cb7b9f36f7f19f8a3451f76a
Mesgun <Mesgun@MESGUN->
2025-10-24 02:41:14 +0200

    Ta bort metoden är klar

--- Automatis/Program.cs
+++ Automatis/Program.cs
@@ -53,12 +53,12 @@ namespace Automatis
                             Paus();
                             break;
                         case "3":
-                            LäggTillBill(db);
+                            LäggTillBil(db);
                             Paus();
                             break;
                         case "4":
-                            TaBort();
-                            Pause();
+                            TaBort(db);
+                            Paus();
                             break;
                         case "5":
                             kör = false;
@@ -158,6 +158,8 @@ namespace Automatis
         {
             Console.WriteLine("Test: lägg till ny kund med bil");
 
+            
+
         }
 
         // Lägg till en till bil
@@ -165,16 +167,132 @@ namespace Automatis
         {
             Console.WriteLine("Test: Lägg till bil till kund");
 
-            var kund =  Väljkund(db);
-            if (kund == null)
+            var Kund =  VäljKund(db);
+            if (Kund == null)
                 return;
+
+            int föreBilar = db.Cars.Count(c=> c.CustomerId == Kund.Id);
+            Console.WriteLine($"Bilar för {Kund.Name} före: {föreBilar}");
+
+            string märke = LäsText("Angel bilmärke: ");
+            string modell = LäsText("Ange bilmodell");
+            int år = LäsInt("Ange årsmodell ): ", 1900, DateTime.Now.Year + 1 );
+
+            var nyBil = new Car { Brand = märke, Model = modell, Year = år, CustomerId = Kund.Id };
+            db.Cars.Add(nyBil);
+            db.SaveChanges();
+
+            int efeterBilar = db.Cars.Count(c => c.CustomerId == Kund.Id);
+            Console.WriteLine($"klar! Bilar för {Kund.Name} efter: {efeterBilar}");
         }
 
+        // ta bort kund eller bil
+        static void TaBort(AppDbContext db)
+        {
+            Console.WriteLine("Vill du ta bort (1) kund eller (2) bil");
+            var val = Console.ReadLine();
+
+            if (val == "1")
+            {
+                var kund = VäljKund(db);
+                if (kund == null) return;
+
+                int föreKunder = db.Customers.Count();
+                db.Customers.Remove(kund);
+                db.SaveChanges();
+                int efterKunder = db.Customers.Count();
+
+                Console.WriteLine($"Tog bort kund {kund.Name} före {föreKunder}, Efter: {efterKunder}");
+
+            }
+            else if (val == "2")
+            {
+                var bilar = db.Cars.ToList();
+                if(bilar.Count == 0)
+                {
+                    Console.WriteLine("Det finns inga bilar att ta bort.");
+                    return;
+                }
+                Console.WriteLine("Välj bil att ta bort");
+                for (int i = 0; i < bilar.Count; i++)
+                {
+                    var owner = db.Customers.FirstOrDefault(c => c.Id == bilar[i].CustomerId);
+                    Console.WriteLine($"{i + 1}. {bilar[i].Brand} {bilar[i].Model} ({bilar[i].Year}) - Ägare: {owner?.Name}");
+
+                }
+                int index = LäsInt("Ange bilnummer: ", 1, bilar.Count) - 1;
+                var bil = bilar [index];
+
+                int föreBilar = db.Cars.Count();
+                db.Cars.Remove(bil);
+                db.SaveChanges();
+                int efterBilar = db.Cars.Count();
+
+                Console.WriteLine($"Tog bort bil {bil.Brand} {bil.Model}. Före: {föreBilar}, Efter: {efterBilar}");
+
+            }
+            else
+            {
+                Console.WriteLine("Ogiltigt val");
+            }
+        }
+
+        // läser textinmattning och ser till att det inte är tomt
+        static string LäsText(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine() ?? "";
+                if(!string.IsNullOrWhiteSpace(input))
+                    return input.Trim();
+
+                Console.WriteLine("Texten får inte vara tom, försök igen");
+            }
+        }
+        // läser ett heltal och kontrollerar att det är giltigt
+        static int LäsInt(string prompt, int min, int max)
+        {
+            while(true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+
+                if(int.TryParse(input, out int tal))
+                {
+                    if (tal >= min && tal <= max)
+                        return tal;
+                }
+
+                Console.WriteLine($"Skriv ett tal mellan {min} och {max}.");
+
+            }
+        }
+
+        // Väljer kund från listan
+        static Customer? VäljKund(AppDbContext db)
+        {
+            var kunder = db.Customers.ToList(); 
+            if (kunder.Count == 0)
+            {
+                Console.WriteLine("Det finns inga kunder");
+                return null;
+            }
+            Console.WriteLine("Välj kund:");
+            for(int i = 0; i < kunder.Count; i++)
+                Console.WriteLine($"{i + 1}. {kunder[i].Name}");
+
+            int index = LäsInt("Ange kundnummer:", 1, kunder.Count) - 1;
+            return kunder[index];
+        }
 
         static void Paus()
         {
+            // jag la till denna metod själv för att kunna pausa programmet 
+            // för att hinna se resultaten inna skärmen rensas
             Console.WriteLine("\nTryck på valfri tanget för att fortsätta.");
             Console.ReadKey();
         }
     }
 }
+
